51c51ade62bedaed16b4b54e132a14d5e74732d6
Kareem Hesham <kareemhesham105@gmail.com>
2026-02-03 00:08:15 +0200

    feature: add CI Pipeline

---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("Hello, DevOps!");
+console.log("I am learning about CI/CD pipelines using Github Actions.");
---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Starting tests...");
+
+setTimeout(() => {
+    console.log("Waiting 3 seconds...");
+}, 3000);
+
+console.log("Tests passed!!...");
+
